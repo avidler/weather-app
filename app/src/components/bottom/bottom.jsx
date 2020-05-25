9ef4afefc7e15d797ad52ee5473fad53ca3c5142
avidler <a.vidler@live.co.uk>
2020-05-25 17:26:22 +0100

    Finished project

--- app/src/components/bottom/bottom.jsx
+++ app/src/components/bottom/bottom.jsx
@@ -10,14 +10,37 @@ function BottomSection(props) {
     console.log("forecastList: ", forecastList)
     return(
         <div className="bottom-container">
-                  <div className="bottom-inner-container">
+            <div className="bottom-container-header">5-day forecast</div>
+            <div className="bottom-inner-container tooltip">
+            
             {
             forecastList.map((day, idx) => {
                 var timeCheck = day.dt_txt.slice(day.dt_txt.length-8)
                 var iconcode = day.weather[0].icon;
-                
+                var dayId = day.dt
+                var forecastDate = day.dt_txt.slice(8, 10)+"/"+day.dt_txt.slice(5, 7);
+                var temp_max = day.main.temp_max
+                var humidity = day.main.humidity
+                var pressure = day.main.pressure
+                var description = day.weather[0].description
+                var wind = day.wind.speed
                 if (timeCheck === "12:00:00"){
-                    return <Forecastday iconcode={iconcode} weather={day.weather[0].main} key={idx}/>
+                    return (
+                        <Forecastday 
+                            iconcode={iconcode} 
+                            weather={day.weather[0].main} 
+                            description={description}
+                            dayId={dayId}
+                            temp_max={temp_max} 
+                            forecastDate={forecastDate}
+                            key={idx}
+                            humidity={humidity}
+                            pressure={pressure}
+                            wind={wind}
+                        />
+                    )
+                   
+                    
     }
     else{return<></>}
 })
--- app/src/components/bottom/forecastday.jsx
+++ app/src/components/bottom/forecastday.jsx
@@ -1,11 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 
 function ForecastDay(props) {
+
+    const [shown, setShown] = useState(false)
+
+    function showToolTip(dayId) {
+        setShown(true)
+    }
+
+    function hideToolTip(dayId) {
+        setShown(false)
+    }
+
     const iconurl = "http://openweathermap.org/img/w/" + props.iconcode + ".png"
-    
-    return(
-        <div className="forecastday-container">
- 
+     return(
+        <div className="forecastday-container" onMouseEnter={() => showToolTip(props.dayId)} onMouseLeave={() => hideToolTip(props.dayId)}>
+                {shown ? <span className="tooltiptext" id={props.dayId}>
+                            {props.description}
+                            <br /><br />Max Temp: {Math.round(props.temp_max)}°
+                            <br />Humidity: {props.humidity}
+                            <br />Pressure: {props.pressure}
+                            <br />Wind Speed: {props.wind}
+                        </span> 
+                        : 
+                        null
+                }
+                <div className="forecastday-container-date">{props.forecastDate}</div>
                 <div className="forecastday-container-image"><img src={iconurl} alt="weather icon"/></div>
                 <div className="forecastday-container-text">{props.weather}</div>
 

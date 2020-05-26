b4f4bc9336fe55554f9735cfdafb2b85601389f1
avidler <a.vidler@live.co.uk>
2020-05-26 20:55:17 +0100

    production build

---
+++ app/build/precache-manifest.35dd1a7b789ee0efdaf086a1cb0ace43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db6838cf50346dee6037fd275919fae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a53a841988c1d544cb5",
+    "url": "/static/css/main.1094d2b2.chunk.css"
+  },
+  {
+    "revision": "e7e01db6f7630dc60336",
+    "url": "/static/js/2.3a199f0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a199f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a53a841988c1d544cb5",
+    "url": "/static/js/main.3a9bbaaa.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  },
+  {
+    "revision": "47590d770b0ee4a047b8230f52027a95",
+    "url": "/static/media/sunny-weather.47590d77.jpg"
+  }
+]);
